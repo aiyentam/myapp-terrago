36038e2a28a640af92797e982131ed657d816e2c
aiyentam <aiyen1991@gmail.com>
2018-05-25 08:54:07 -0400

    targeting value and functionality

--- src/main/Main.js
+++ src/main/Main.js
@@ -3,14 +3,41 @@ import Columns from "../columns/Columns";
 import "./Main.css";
 
 class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numOfCol: 3,
+      height: 760
+    };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(e) {
+    console.log("name: " + e.target.name);
+    console.log("value: " + e.target.value);
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
   render() {
     return (
       <div className="main">
         <form className="column-amount">
           <label>Number of columns</label>
-          <input type="number" name="numOfCol" />
+          <input
+            type="number"
+            name="numOfCol"
+            value={this.state.numOfCol}
+            onChange={this.onInputChange}
+          />
           <label>Row height</label>
-          <input type="number" name="height" placeholder="px" />
+          <input
+            type="number"
+            name="height"
+            value={this.state.height}
+            onChange={this.onInputChange}
+          />px
         </form>
         <Columns />
       </div>
